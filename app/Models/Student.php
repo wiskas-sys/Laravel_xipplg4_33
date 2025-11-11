3ec2fd346014559f47fa82a194386f5240a02d8d
wiskas-sys <wsinduadi@gmail.com>
2025-11-11 13:04:24 +0700

    Add Feature Create Student

---
+++ app/Models/Student.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+
+class Student extends Model
+{
+
+    use HasFactory;
+
+    protected $fillable = [
+    'nis',
+    'nama_lengkap',
+    'jenis_kelamin',
+    'nisn',
+
+    ];
+}
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Landingcontroller;
+use App\Http\Controllers\LandingController;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\StudentController;
 
-Route::get('/', [LandingController::class, 'index' ])->name('landing' );
+Route::get ('/', [LandingController::class, 'index' ] )->name('landing' );
 
-Route::prefix('admin')->group(function () {
-    Route::get ('/dashboard', [DashboardController::class, 'index' ] )->name('admin. dashboard' );
+Route:: prefix ('admin' )->name('admin.')->group(function () {
+Route:: get ('/dashboard', [DashboardController:: class, 'index' ] )->name ('dashboard' );
+
+// Untuk CRUD table Student
+Route:: resource ('students', StudentController:: class);
 
 });
